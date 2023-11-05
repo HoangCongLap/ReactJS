adbee587bc54953668ede3780ea33bd32320f450
Lap <lapconghoang@gmail.com>
2023-11-05 21:19:00 +0700

    eventBasics

--- book-list/src/index.js
+++ book-list/src/index.js
@@ -2,36 +2,64 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 
+const books = [
+  {
+    author: "Scott Pratt",
+    title: "An Innocent Client",
+    img: "./images/book-1.jpg",
+    id: 1,
+  },
+  {
+    author: " Hernan Diaz",
+    title: "Trust",
+    img: "./images/book-2.jpg",
+    id: 2,
+  },
+];
+
 const BookList = () => {
   return (
     <section className="booklist">
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      <EventExamples />
+      {books.map((book) => {
+        return <Book {...book} key={book.id} />;
+      })}
     </section>
   );
 };
-const Book = () => {
+
+const EventExamples = () => {
+  const handleFormInput = () => {
+    console.log("handle form input");
+  };
+  const handleButtonClick = () => {
+    alert("handle button click");
+  };
   return (
-    <article className="book">
-      <Image />
-      <Title />
-      <Author />
-    </article>
+    <section>
+      <form>
+        <h2>Typical form</h2>
+        <input
+          type="text"
+          name="example"
+          onChange={handleFormInput}
+          style={{ margin: "1rem 0" }}
+        />
+      </form>
+      <button onClick={handleButtonClick}>click me</button>
+    </section>
   );
 };
 
-const Image = () => <img src="./images/book-1.jpg" alt="An Innocent Client" />;
-const Title = () => <h2>An Innocent Client</h2>;
-const Author = () => {
-  const inlineHeadingStyles = {
-    color: "#617d98",
-    fontSize: "0.75rem",
-    marginTop: "0.5rem",
-  };
-
-  return <h4 style={inlineHeadingStyles}>Scott Pratt</h4>;
+const Book = (props) => {
+  const { img, title, author } = props;
+  return (
+    <article className="book">
+      <img src={img} alt={title} />
+      <h2>{title}</h2>
+      <h4>{author}</h4>
+    </article>
+  );
 };
 
 const root = createRoot(document.getElementById("root"));
