a8aefb9fdc77b4507f6f0131e67e67287d8a08a7
Lap <lapconghoang@gmail.com>
2023-11-07 19:31:15 +0700

    51_book_list

---
+++ book-list/src/books.js
@@ -0,0 +1,23 @@
+import img1 from "./images/book-1.jpg";
+import img2 from "./images/book-2.jpg";
+import img3 from "./images/book-3.jpg";
+export const books = [
+  {
+    author: "Scott Pratt",
+    title: "An Innocent Client",
+    img: img1,
+    id: 1,
+  },
+  {
+    author: "Hernan Diaz",
+    title: "Trust",
+    img: img2,
+    id: 2,
+  },
+  {
+    author: "Maren Foster",
+    title: "THE VIRTUOUS CON",
+    img: img3,
+    id: 3,
+  },
+];
--- book-list/src/index.js
+++ book-list/src/index.js
@@ -2,63 +2,18 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 
-const books = [
-  {
-    author: "Scott Pratt",
-    title: "An Innocent Client",
-    img: "./images/book-1.jpg",
-    id: 1,
-  },
-  {
-    author: " Hernan Diaz",
-    title: "Trust",
-    img: "./images/book-2.jpg",
-    id: 2,
-  },
-];
-
+import { books } from "./books";
+import Book from "./book";
 const BookList = () => {
   return (
-    <section className="booklist">
-      <EventExamples />
-      {books.map((book) => {
-        return <Book {...book} key={book.id} />;
-      })}
-    </section>
-  );
-};
-
-const EventExamples = () => {
-  const handleFormInput = () => {
-    console.log("handle form input");
-  };
-  const handleButtonClick = () => {
-    alert("handle button click");
-  };
-  return (
-    <section>
-      <form>
-        <h2>Typical form</h2>
-        <input
-          type="text"
-          name="example"
-          onChange={handleFormInput}
-          style={{ margin: "1rem 0" }}
-        />
-      </form>
-      <button onClick={handleButtonClick}>click me</button>
-    </section>
-  );
-};
-
-const Book = (props) => {
-  const { img, title, author } = props;
-  return (
-    <article className="book">
-      <img src={img} alt={title} />
-      <h2>{title}</h2>
-      <h4>{author}</h4>
-    </article>
+    <>
+      <h1>amzon best sellers</h1>
+      <section className="booklist">
+        {books.map((book, index) => {
+          return <Book {...book} key={book.id} number={index} />;
+        })}
+      </section>
+    </>
   );
 };
 
