c1f79dfd9280a96f2d8356c9fe9b95462e643c13
Lap <lapconghoang@gmail.com>
2023-11-12 19:24:54 +0700

    63_backroadsApp

--- backroads-app/src/App.js
+++ backroads-app/src/App.js
@@ -1,7 +1,21 @@
 import "./App.css";
-
+import About from "./components/About";
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import Services from "./components/Services";
+import Tours from "./components/Tours";
 function App() {
-  return <h1>Backroads app</h1>;
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <About />
+      <Services />
+      <Tours />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
