ffb915f0024b8084c66bc168a00cc433c4ddf908
Lap <lapconghoang@gmail.com>
2024-01-22 10:12:41 +0700

    new

---
+++ react-course-v3-main/02-backroads-project/final/src/components/Hero.js
@@ -0,0 +1,14 @@
+const Hero = () => {
+  return (
+    <section className="hero" id="home">
+      <div className="hero-banner">
+        <h1>backroads app</h1>
+        <p>Ghi nhớ 1000 từ vựng trong 1 tháng</p>
+        <a href="#tours" className="btn hero-btn ">
+          explore tours
+        </a>
+      </div>
+    </section>
+  );
+};
+export default Hero;
